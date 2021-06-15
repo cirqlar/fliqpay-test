284eea855067f4b6c4137b4d66ec7510b3507059
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-06-16 00:17:31 +0100

    feat: add generic multiStepForm component

---
+++ src/components/forms/multiStepForm/index.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback, useMemo, useState } from "react";
+
+import StepSlider from "./stepSlider";
+
+export type MultiStepChildProps = {
+  goToNext: (data?: any) => void;
+  data?: any;
+  [key: string]: any;
+};
+
+export type StepType = [string, React.ComponentType<MultiStepChildProps>];
+
+type MultiStepFormProps = {
+  steps: StepType[];
+  currentStep?: number;
+  executeAfter: (data: any) => any;
+};
+
+function MultiStepForm({ steps, currentStep: cs, executeAfter }: MultiStepFormProps) {
+  const [currentStep, setCurrentStep] = useState(cs ?? 0);
+  const [currentComponentData, setCurrentComponentData] = useState<any>();
+  const [numberOfSteps] = useState(steps.length);
+
+  const stepNames = useMemo(() => {
+    return steps.map(([stepName]) => stepName);
+  }, [steps]);
+
+  const CurrentComponent = useMemo(() => {
+    console.log("Our component", currentStep);
+    return steps[currentStep][1];
+  }, [currentStep, steps]);
+
+  const goToNextStep = useCallback(
+    (data?: any) => {
+      console.log("It was called", currentStep, numberOfSteps);
+      if (currentStep < numberOfSteps - 1) {
+        setCurrentStep(currentStep + 1);
+        setCurrentComponentData(data);
+      } else {
+        executeAfter && executeAfter(data);
+      }
+    },
+    [currentStep, executeAfter, numberOfSteps]
+  );
+
+  return (
+    <>
+      <StepSlider steps={stepNames} current={currentStep} />
+      <div className="px-5 mt-7 bg-white sm:max-w-lg sm:mx-auto sm:border sm:border-gray-light sm:px-8 sm:pt-8 sm:pb-12">
+        <CurrentComponent data={currentComponentData} goToNext={goToNextStep} />
+      </div>
+    </>
+  );
+}
+
+export default MultiStepForm;
---
+++ src/components/forms/multiStepForm/multiStepForm.test.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
+
+import MultiStepForm, { StepType } from ".";
+
+const steps: StepType[] = [
+  ['Component One', ({ goToNext }) => (
+    <div>
+      <span>Component One Text</span>
+      <button onClick={() => goToNext("Component One Data")}>Component One Button</button>
+    </div>
+  )],
+  ['Component Two', ({ data, goToNext }) => (
+    <div>
+      <span>Component Two Text</span>
+      <span data-testid="dataTwo">{ data }</span>
+      <button onClick={() => goToNext("Component Two Data")}>Component Two Button</button>
+    </div>
+  )],
+  ['Component Three', ({ data, goToNext }) => (
+    <div>
+      <span>Component Three Text</span>
+      <span data-testid="dataThree">{ data }</span>
+      <button onClick={() => goToNext("Component Three Data")}>Component Three Button</button>
+    </div>
+  )],
+]
+
+it("should render steps correctly", () => {
+  const executeAfter = jest.fn();
+  act(() => {
+    render(<MultiStepForm steps={steps} executeAfter={executeAfter} />);
+  });
+
+  expect(screen.getByText("Component One Text")).toBeInTheDocument();
+  fireEvent.click(screen.getByText("Component One Button"));
+
+  expect(screen.getByText("Component Two Text")).toBeInTheDocument();
+  expect(screen.getByTestId("dataTwo").textContent).toBe("Component One Data");
+  fireEvent.click(screen.getByText("Component Two Button"));
+
+  expect(screen.getByText("Component Three Text")).toBeInTheDocument();
+  expect(screen.getByTestId("dataThree").textContent).toBe("Component Two Data");
+  fireEvent.click(screen.getByText("Component Three Button"));
+
+  expect(executeAfter).toHaveBeenCalledWith("Component Three Data");
+})
