036d5f9d2bdd23f11bfc543d96b00d41385a5e39
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-06-18 02:21:43 +0100

    feat: rename payoutForm folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import DefaultLayout from "../components/layout/defaultLayout";
-import PayoutForm from "../components/forms/PayoutForm";
+import PayoutForm from "../components/forms/payoutForm";
 
 function Index() {
   return (
