d83e5c226a48a48ee6e9d90f1b9c8cd4f4aaebe5
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-06-18 02:23:42 +0100

    feat: reorganize pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Index from './pages/index';
+import Payout from './pages/payout';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Index />
+    <Payout />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/payout/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import DefaultLayout from "../../components/layout/defaultLayout";
+import PayoutForm from "../../components/forms/payoutForm";
+
+function Payout() {
+  return (
+    <DefaultLayout>
+      <PayoutForm />
+    </DefaultLayout>
+  );
+}
+
+export default Payout;
